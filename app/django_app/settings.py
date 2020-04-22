c1f6f33d7d3c69efaa7a5a63ebdbe7149e0c34ef
Sam Prasad <samridhprasad@gmail.com>
2020-04-22 19:55:02 -0400

    Added deploy workflow

--- app/django_app/settings.py
+++ app/django_app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
