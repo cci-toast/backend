e6f2943eb5e70e8825e59c012971ea2c61fd8d5e
Sam Prasad <samridhprasad@gmail.com>
2020-04-24 15:53:06 -0400

    tech-debt: Refactored urls for clients app and the main django_app

--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -14,12 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from clients.models import Partner, Expense, Children, Goal, Plan, Debt, ActionItem
-from clients.serializers import PartnerSerializer, \
-    ExpenseSerializer, ChildrenSerializer, GoalSerializer, \
-    PlanSerializer, DebtSerializer, ActionItemSerializer
-from clients.views import AdvisorView, ClientView, ClientDependModelView
+from django.urls import path, include
 from django.http import JsonResponse
 
 
@@ -29,30 +24,6 @@ def home(request):
 
 urlpatterns = [
     path('', home, name='home'),
-    path('admin/', admin.site.urls),
+    path(r'api/', include('clients.urls')),
 
-    path('api/advisors', AdvisorView.as_view()),
-
-    path('api/clients', ClientView.as_view()),
-
-    path('api/expenses', ClientDependModelView.as_view(
-        queryset=Expense.objects.all(), serializer_class=ExpenseSerializer)),
-
-    path('api/partner', ClientDependModelView.as_view(
-        queryset=Partner.objects.all(), serializer_class=PartnerSerializer)),
-
-    path('api/children', ClientDependModelView.as_view(
-        queryset=Children.objects.all(), serializer_class=ChildrenSerializer)),
-
-    path('api/goals', ClientDependModelView.as_view(
-        queryset=Goal.objects.all(), serializer_class=GoalSerializer)),
-
-    path('api/plan', ClientDependModelView.as_view(
-        queryset=Plan.objects.all(), serializer_class=PlanSerializer)),
-
-    path('api/debt', ClientDependModelView.as_view(
-        queryset=Debt.objects.all(), serializer_class=DebtSerializer)),
-
-    path('api/action_items', ClientDependModelView.as_view(
-        queryset=ActionItem.objects.all(), serializer_class=ActionItemSerializer)),
 ]
