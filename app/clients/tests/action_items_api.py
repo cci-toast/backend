c5a5a5c0f31246f405c1edadfda969e0cbf1f2a8
bao tran <baotran@baos-MacBook-Pro.local>
2020-05-05 21:06:29 -0400

    fix some tests failed because of refactoring in the API url

--- app/clients/tests/action_items_api.py
+++ app/clients/tests/action_items_api.py
@@ -7,42 +7,42 @@ from rest_framework.test import APITestCase
 class ActionItemAPITest(APITestCase):
     def setUp(self):
         self.expected_action_items = []
-        #create a client 
-        response = self.client.generic('POST', '/api/clients', json.dumps({
+        # create a client
+        response = self.client.post('/api/clients', data={
             'first_name': 'Kai',
             'last_name': 'Anderson',
             'birth_year': '1990',
             'email': 'kai.anderson@gmail.com',
             'personal_annual_net_income': 800000.0,
             'additional_income': 10000.0
-            }), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response_data = json.loads(response.content)
         self.first_client_id = response_data['id']
 
-        #create first action item 
-        response = self.client.generic('POST', '/api/action_items', json.dumps({
+        # create first action item
+        response = self.client.post('/api/action_items', data={
             'client': self.first_client_id,
             'description': 'Set aside $1,600 for emergency savings',
             'completed': False
-            }), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response_data = json.loads(response.content)
         self.first_action_item_id = response_data['id']
-        
+
         self.expected_action_items.append({
             'id': self.first_action_item_id,
             'description': 'Set aside $1,600 for emergency savings',
             'completed': False
         })
 
-        #create second action item
-        response = self.client.generic('POST', '/api/action_items', json.dumps({
+        # create second action item
+        response = self.client.post('/api/action_items', data={
             'client': self.first_client_id,
             'description': 'Set aside $500 to repay debt this month',
             'completed': True
-            }), content_type='application/json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response_data = json.loads(response.content)
         self.second_action_item_id = response_data['id']
 
@@ -50,76 +50,59 @@ class ActionItemAPITest(APITestCase):
             'id': self.second_action_item_id,
             'description': 'Set aside $500 to repay debt this month',
             'completed': True
-        })   
-
-
+        })
 
     def test_post(self):
-        response = self.client.generic('POST', '/api/action_items', json.dumps({
+        response = self.client.post('/api/action_items', data={
             'client': self.first_client_id,
             'description': 'Set aside $500 to repay debt this month',
             'completed': True
-            }), content_type='application/json')
+        })
         response_data = json.loads(response.content)
-        
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response_data, {
             'id': response_data['id'],
             'description': 'Set aside $500 to repay debt this month',
             'completed': True
         })
-    
 
     def test_get_list_of_items(self):
-        #get all action items for a client
-        response = self.client.generic('GET', '/api/action_items', json.dumps({
-            'client': self.first_client_id
-        }), content_type='application/json')
+        # get all action items for a client
+        response = self.client.get('/api/action_items')
         response_data = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response_data, self.expected_action_items)
-    
+        self.assertEqual(response_data, {
+            'count': 2,
+            'next': None,
+            'previous': None,
+            'results': self.expected_action_items
+        })
+
     def test_get_action_item_with_valid_id(self):
-        #get action item by id 
-        response = self.client.generic('GET', '/api/action_items', json.dumps({
-            'id': self.first_action_item_id,
-            'client': self.first_client_id
-        }), content_type='application/json')
+        # get action item by id
+        response = self.client.get(
+            '/api/action_items/' + self.first_action_item_id)
         response_data = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response_data, self.expected_action_items[0])       
+        self.assertEqual(response_data, self.expected_action_items[0])
 
     def test_get_action_item_with_non_exist_id(self):
-        #get non-existent action item 
-        response = self.client.generic('GET', '/api/action_items', json.dumps({
-            'id': '123e4567-e89b-12d3-a456-426655440000',
-            'client': self.first_client_id
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response_data, {
-            'id': ['Object not exists.']
-        })
-    
+        # get non-existent action item
+        response = self.client.get(
+            '/api/action_items/123e4567-e89b-12d3-a456-426655440000')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_get_action_item_with_invalid_id(self):
-        #get actiion item with invalid id 
-        response = self.client.generic('GET', '/api/action_items', json.dumps({
-            'id': '50392',
-            'client': self.first_client_id
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response_data, {
-            'id': ['Must be a valid UUID.']
-        })
+        # get actiion item with invalid id
+        response = self.client.get('/api/action_items/50392')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_patch_item_with_valid_id(self):
-        #patch an action item by id 
-        response = self.client.generic('PATCH', '/api/action_items', json.dumps({
-            'id': self.first_action_item_id,
-            'client': self.first_client_id,
+        # patch an action item by id
+        response = self.client.patch('/api/action_items/' + self.first_action_item_id, data={
             'completed': True
-        }), content_type='application/json')
+        })
         response_data = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -127,74 +110,45 @@ class ActionItemAPITest(APITestCase):
         expected_change['completed'] = True
         self.assertEqual(response_data, expected_change)
 
-
     def test_patch_item_with_non_exist_id(self):
-        #patch non-exsitent item 
-        response = self.client.generic('PATCH', '/api/action_items', json.dumps({
-            'id': '123e4567-e89b-12d3-a456-426655440000',
-            'client': self.first_client_id,
+        # patch non-exsitent item
+        response = self.client.patch('/api/action_items/123e4567-e89b-12d3-a456-426655440000', data={
             'completed': True
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response_data, {
-            'id': ['Object not exists.']
         })
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_patch_item_with_invalid_id(self):
-        #patch action item with invalid id
-        response = self.client.generic('PATCH', '/api/action_items', json.dumps({
-            'id': '12345',
-            'client': self.first_client_id,
+        # patch action item with invalid id
+        response = self.client.patch('/api/action_items/12345', data={
             'completed': True
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response_data, {
-            'id': ['Must be a valid UUID.']
         })
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_item_with_valid_id(self):
-        #delete action item with id 
-        response = self.client.generic('DELETE', '/api/action_items', json.dumps({
-            'id': self.first_action_item_id,
-            'client': self.first_client_id
-        }), content_type='application/json')
+        # delete action item with id
+        response = self.client.delete(
+            '/api/action_items/' + self.first_action_item_id)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # make sure we only have one action item left
+        response = self.client.get(
+            '/api/action_items?client=' + self.first_client_id)
         response_data = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        expected_change = self.expected_action_items[0]
-        expected_change['id'] = None
-        self.assertEqual(response_data, expected_change)
-
-        # make sure we only have one action item left 
-        response = self.client.generic('GET', '/api/action_items', json.dumps({
-            'client': self.first_client_id
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response_data, [self.expected_action_items[1]])
+        self.assertEqual(response_data, {
+            'count': 1,
+            'next': None,
+            'previous': None,
+            'results': [self.expected_action_items[1]]
+        })
 
     def test_delete_with_non_exist_id(self):
         # delete non-existent action item
-        response = self.client.generic('DELETE', '/api/action_items', json.dumps({
-            'id': '123e4567-e89b-12d3-a456-426655440000',
-            'client': self.first_client_id
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response_data, {
-            'id': ['Object not exists.']
-        })
+        response = self.client.delete(
+            '/api/action_items/123e4567-e89b-12d3-a456-426655440000')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_with_invalid_id(self):
-        # delete with invalid id
-        response = self.client.generic('DELETE', '/api/action_items', json.dumps({
-            'id': '223311',
-            'client': self.first_client_id
-        }), content_type='application/json')
-        response_data = json.loads(response.content)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response_data, {
-            'id': ['Must be a valid UUID.']
-        })
-        
+        # delete non-existent action item
+        response = self.client.delete('/api/action_items/12345')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
