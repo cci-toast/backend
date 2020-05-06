5cdac14b2021c7078a53ef5909f9d136ffdb8f8e
Sam Prasad <samridhprasad@gmail.com>
2020-05-06 12:53:34 -0400

    Removed path to manage.py to avoid repeated app/ in staticfiles directory

--- app/django_app/settings.py
+++ app/django_app/settings.py
@@ -149,8 +149,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
