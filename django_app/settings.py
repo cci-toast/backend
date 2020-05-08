dc20f2336704b9be8a456dbe9408c4e0f3c7d50f
Sam Prasad <samridhprasad@gmail.com>
2020-05-08 12:34:02 -0400

    Got rid of int()

--- django_app/settings.py
+++ django_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY", default='superfoosecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=True))
+DEBUG = (os.environ.get("DEBUG", default=True))
 
 ALLOWED_HOSTS = ['toastbackend.herokuapp.com']
 # Help guard against XSS attacks
