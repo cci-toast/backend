8175d8b8272b8fa1fcae4210b1c9ab86991040da
Minji Hong <mh3233@drexel.edu>
2020-05-17 14:05:44 -0700

    added unit tests for goal, partner, and expense

--- toastapi/tests/__init__.py
+++ toastapi/tests/__init__.py
@@ -1,3 +1,6 @@
+from .test_expense_api import *
+from .test_goal_api import *
+from .test_partner_api import *
 from .test_advisor_api import *
 from .test_children_api import *
 from .test_client_api import *
